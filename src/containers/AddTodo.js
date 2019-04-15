c1f850c6326be1e7056197a7138cd757cd4f53b4
Dexter Hennington <dextermichael.h@gmail.com>
2019-04-15 14:08:52 -0400

    add boxes

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default class AddTodo extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>AddTodo</Text>
+      <View style={{ flexDirection: "row", marginHorizontal: 20 }}>
+        <TextInput
+          placeholder="Ex.Create New Video"
+          style={{
+            borderWidth: 1,
+            borderColor: "#f2f2e1",
+            backgroundColor: "eaeaea",
+            height: 50,
+            flex: 1,
+            padding: 5
+          }}
+        />
+        <TouchableOpacity onPress={() => alert("added too")}>
+          <View style={{ height: 50, backgroundColor: "eaeaea" }}>
+            <Ionicons name="md-add" size={30} />
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
